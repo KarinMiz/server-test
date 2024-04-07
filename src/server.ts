dc1670642590388217b1ae27e4904cc073aca18d
KarinMiz <karinush1803@gmail.com>
2024-04-07 23:52:11 +0300

    remove websocket

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser';
 import { data } from './data';
 import publishersRoutes from './routes/publishersRoutes';
 import domainsRoutes from './routes/domainsRoutes';
-// import { startWebSocketServer } from './webSocket';
 
 const app = express();
 app.use(cors({
@@ -18,8 +17,6 @@ const port = 3000;
 
 app.use('/api/publishers', publishersRoutes);
 app.use('/api/domains', domainsRoutes);
-
-// startWebSocketServer();
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
